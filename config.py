53fc925bf01c7db95aa1977e66a10d8d16e703f2
Shabnam <nazmi.shabnam@gmail.com>
2020-01-24 14:41:43 -0500

    cleaned up config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ NUMBER_OF_FOLDS = 1                #k-fold cross-validation model training
 DO_AVERAGING = False               #reoprt averages performance
 
 
-DATA_HEADER = "flags"
-NO_ATTRIBUTES = 19
+DATA_HEADER = "medical"
+NO_ATTRIBUTES = 1449
 VALID_DATA_HEADER = DATA_HEADER + "-test"
 TRAIN_DATA_HEADER = DATA_HEADER + "-train"
 DATA_FOLDER = "D:\Datasets"
@@ -12,16 +12,16 @@ RUN_RESULT_PATH = "Run_results_MLRBC"
 
 
 NO_TRAIN_ITERATION = [30000]
-POP_SIZE = [500] #[500,1000,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000]
-P_HASH = [0.5]  #[0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9]
-THETA_GA = [50] #[10,15,20,25,30,35,40,45,50,55,60]
-NU = [10] #[5,10,15,20,25,30,35,40,45,50,55,60]
+POP_SIZE = [5000]
+P_HASH = [0.5]
+THETA_GA = [50]
+NU = [10]
 NO_EXPERIMENTS_AVERAGING = 10      #repeat the training to reduce the variance
 NO_PARALLEL_JOBS = NO_EXPERIMENTS_AVERAGING              #number of threads to perform parallel model training
 
 
 DISTRIBUTED_MATCHING_TH = 2600000   
-REDUCE_ATTRIBUTE = None              #feature selection parameter (1, <1, None). 1: considers feature-list.txt
+REDUCE_ATTRIBUTE = 1              #feature selection parameter (1, <1, None). 1: considers feature-list.txt
                                   # <1: takes a portion of feature-list.txt, None: no feature selection
 REBOOT_MODEL = 0                  #reboot a previously trained model from the file
 DOWN_SAMPLE_RATIO = 1             #down_sampling ratio. For faster prototype evaluations
